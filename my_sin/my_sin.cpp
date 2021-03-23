05cfa9c655de168deef4406e80c0d02b6c9ea252
slavaqweqwe <74615552+slavaqweqwe@users.noreply.github.com>
2021-03-23 19:37:11 +0300

    2

--- my_sin/my_sin.cpp
+++ my_sin/my_sin.cpp
@@ -2,27 +2,20 @@
 //
 
 #include "my_sin.h"
-#define twoPI 6.2832
+constexpr double twoPI = 6.2832;
 using namespace std;
 
 double testspace::Sin(double arg) {
-	arg = arg - twoPI * int(arg / (twoPI));
-	double arg_in_two = arg * arg;
-	double res = 0, res_next = arg;
-	double degree = 1;
-	double  numerator = arg, denominator = 1;
-	bool next_sign = 1;
-	while (abs(res_next - res) > 0.0001) {
-		res = res_next;
 
+	double arg_in_two = arg * arg;
+	double value_for_add = arg;
+	double res  = arg;
+	double degree = 1;
+	while (abs(value_for_add) > 0.0001) {
+		
 		degree += 2;
-		numerator *= arg_in_two;
-		denominator *= (degree * (degree - 1));
-		if (next_sign)
-			res_next -= numerator / denominator;
-		else
-			res_next += numerator / denominator;
-		next_sign = !next_sign;
+		value_for_add *= -1.0 * arg_in_two / (degree * (degree - 1));
+		res_next+= value_for_add
 	}
 	return res_next;
 }
